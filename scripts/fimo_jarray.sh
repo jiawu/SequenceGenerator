3d4e5c70020f762d9c0ca73e4288af9c6f67edb7
WU Jia <jjw036@quser02.(none)>
2014-09-21 13:32:03 -0500

    fixed many bugs including the submission script and the job array script. the sort now should sort in a descending fashion

--- scripts/fimo_jarray.sh
+++ scripts/fimo_jarray.sh
@@ -4,20 +4,39 @@
 #MSUB -l nodes=1:ppn=1
 #MSUB -j oe
 #MSUB -M jiawu@u.northwestern.edu
-#MSUB -N P53_$nbatch
 #MSUB -e FIMO_error_file.err
-#MSUB -o FIMO_log_file_p53.log
+#MSUB -o FIMO_log_file_GATA1_01.log
 #MSUB -m bae
 #MSUB -q normal
 
+export R_LIBS="/home/jjw036/R/library"
+export PATH="$PATH:/home/jjw036/.local/bin"
+#:/home/jjw036/meme/bin:
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jjw036/R/library"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jjw036/.local/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jjw036/.local/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/hpc/software"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/jjw036/.local/lib64/R/lib"
+export WORKON_HOME="/home/jjw036/virtualenv"
+export VIRTUALENVWRAPPER_PYTHON="/software/enthought/python/epd_free-7.3.2-rh6-x86_64/bin/python2.7"
+export VIRTUALENVWRAPPER_VIRTUALENV="/home/jjw036/.local/bin/virtualenv"
+. /home/jjw036/.local/bin/virtualenvwrapper.sh
+
+workon seqgen
+module load python/anaconda3
+
 chunk_size=20
 counter=0
-all_filenames=($(find /projects/p20519/jia_output/FIMO/P53_01/P53_01_*.* -type f))
-output_folder="P53_output"
+search_base=GATA1_01
+search=GATA1_01_5_10000000
+all_filenames=($(find /projects/p20519/jia_output/FIMO/${search_base}/${search}.* -type f))
+output_folder="GATA1_output"
 
 nbatch=${MOAB_JOBARRAYINDEX}
 nfiles=${#all_filenames[@]}
 max_batch=$(($(($nfiles+$chunk_size-1))/$chunk_size))
+#MSUB -N GATA1_$nbatch
 
 #this script will generate one job array (500 jobs)
 #how is this different from fimo_script.sh?
@@ -65,43 +84,47 @@ generate_compress_command () {
   local cmd_base_filename=$2
   local cmd_counter=$3
 
-  compress_command="cat /projects/p20519/jia_output/FIMO/${output_folder}/${cmd_base_filename}_job${cmd_counter}.txt | python /home/jjw036/SequenceGenerator/scripts/FimoEvaluator_P53.py"
+  compress_command="cat /projects/p20519/jia_output/FIMO/${output_folder}/${cmd_base_filename}_job${cmd_counter}.txt | python /home/jjw036/SequenceGenerator/scripts/FimoEvaluator.py"
 }
 echo "Processing ${nfiles} files in ${max_batch} batches"
 
 counter=$(($nbatch*$chunk_size))
+
 end_index=0
 file_batch=()
-while [ $nbatch -lt $(($nbatch+1)) ]; do
-  command_list=""
+command_list=""
   
-  end_index=$(($chunk_size+$counter))
+end_index=$(($chunk_size+$counter))
 
-  if [ "${end_index}" -gt "${nfiles}" ]
-  then
-    echo "last batch"
-    end_index=$(($nfiles-1))
-  fi
+if [ "${counter}" -gt "${nfiles}" ]
+then
+  counter=0
+fi
+if [ "${chunk_size}" -gt "${nfiles}" ]
+then
+  chunk_size=${nfiles}
+fi
 
-  file_batch=("${all_filenames[@]:$counter:$chunk_size}"
-  echo "Processing batch $nbatch"
-  for filename in ${file_batch[*]}; do
+if [ "${end_index}" -gt "${nfiles}" ]
+then
+  echo "last batch"
+  end_index=$(($nfiles-1))
+fi
 
-    base_filename=$(basename "$filename")
-    extension="${base_filename##*.}"
-    base_filename="${base_filename%.*}"
+file_batch=("${all_filenames[@]:$counter:$chunk_size}")
+echo "Processing batch $nbatch"
 
-    generate_pipe $filename $base_filename $extension
+for filename in ${file_batch[*]};
+do
+  base_filename=$(basename "$filename")
+  extension="${base_filename##*.}"
+  base_filename="${base_filename%.*}"
 
-    generate_command $filename $base_filename $extension
+  mkfifo /projects/p20519/jia_output/FIMO/${output_folder}/${base_filename}_job${extension}.txt
+  
+  fimo --max-stored-scores 500000000 --thresh 0.0001  --max-seq-length 250000000 --text /projects/p20519/jia_output/TRANSFAC2FIMO_3242014.txt $filename >> /projects/p20519/jia_output/FIMO/${output_folder}/${base_filename}_job${extension}.txt &
 
-    generate compress_command $filename $base_filename $extension
+  cat /projects/p20519/jia_output/FIMO/${output_folder}/${base_filename}_job${extension}.txt | python /home/jjw036/SequenceGenerator/scripts/FimoEvaluator.py
 
-    command_list="$command_list"$'\n'"$pipe_command"$'\n'"$command"$'\n'"$compress_command"
-
-  done
 done
 
-workon seqgen
-module load python/anaconda3
-${command_list}
--- scripts/fimo_script_job_array.sh
+++ scripts/fimo_script_job_array.sh
@@ -22,6 +22,18 @@ max_batch=$(($(($nfiles+$chunk_size-1))/$chunk_size))i
 #this script will generate one job array (500 jobs)
 #how is this different from fimo_script.sh?
 #each chunk = batch
+# need to utilize ${MOAB_JOBARRAYINDEX}
+
+#need to generate job array script for one batch!
+#transform numbers into index
+
+#workflow
+# 1. generate index file for all the files
+# 2. generate general MOAB.pbs file that utilizes indexes, and has 20 commands
+  # a. name of the file should include chunk_size
+
+#submit via msub jobname.[1-end index:chunksize] myscript.pbs
+
 
 generate_pipe () {
   #echo "file name is $1"
@@ -55,4 +67,66 @@ generate_compress_command () {
 
   compress_command="cat /projects/p20519/jia_output/FIMO/${output_folder}/${cmd_base_filename}_job${cmd_counter}.txt | python /home/jjw036/SequenceGenerator/scripts/FimoEvaluator_P53.py"
 }
+echo "Processing ${nfiles} files in ${max_batch} batches"
+
+counter=$(($nbatch*$chunk_size))
+end_index=0
+file_batch=()
+while [ $nbatch -lt $(($max_batch+1)) ]; do
+  command_list=""
+  
+  if [ $(( $nbatch%40 )) -eq 0 ]; then
+    echo "pause"
+    sleep 2m
+  fi
+  if [$(( $nbatch%200 )) -eq 0 ]; then
+    echo "pause"
+    sleep 60m
+  fi
+
+  end_index=$(($chunk_size+$counter))
+
+  if [ "${end_index}" -gt "${nfiles}" ]
+  then
+    echo "last batch"
+    end_index=$(($nfiles-1))
+  fi
+
+  file_batch=("${all_filenames[@]:$counter:$chunk_size}"
+  echo "Processing batch $nbatch"
+  for filename in ${file_batch[*]}; do
+
+    base_filename=$(basename "$filename")
+    extension="${base_filename##*.}"
+    base_filename="${base_filename%.*}"
+
+    generate_pipe $filename $base_filename $extension
+
+    generate_command $filename $base_filename $extension
+
+    generate compress_command $filename $base_filename $extension
+
+    command_list="$command_list"$'\n'"$pipe_command"$'\n'"$command"$'\n'"$compress_command"
+
+  done
+  echo "$command_list"
+
+  cat <<EOS | msub -
+#!/bin/bash
+#MSUB -A p20519
+#MSUB -l walltime=24:00:00
+#MSUB -l nodes=1:ppn=1
+#MSUB -j oe
+#MSUB -M jiawu@u.northwestern.edu
+#MSUB -N P53_$nbatch
+#MSUB -V
+#MSUB -e FIMO_error_file.err
+#MSUB -o FIMO_log_file_p53.log
+#MSUB -m bae
+#MSUB -q normal
+
+workon seqgen
+module load python/anaconda3
+${command_list}
+EOS
 
